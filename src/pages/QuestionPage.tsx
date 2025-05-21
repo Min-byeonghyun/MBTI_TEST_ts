dbeb768f41a1edd963fdf7a14773ed59ebefc182
Min-byeonghyun <qudgus5125@naver.com>
2025-05-21 22:02:19 +0900

    질문페이지 test 로직 작성

--- src/pages/QuestionPage.tsx
+++ src/pages/QuestionPage.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Button } from "react-bootstrap";
 import Header from "../components/Header";
+import { useNavigate } from "react-router-dom";
 import { QuestionData } from "../stores/Question/QuestionData";
 
 const Wrapper = styled.div`
@@ -39,25 +40,53 @@ const ButtonGroup = styled.div`
 
 export default function QuestionPage(): React.ReactElement {
   const [questionNo, setQuestionNo] = useState(0);
+  const [totalScore, setTotalScore] = useState([
+    { id: "EI", score: 0 },
+    { id: "SN", score: 0 },
+    { id: "TF", score: 0 },
+    { id: "JP", score: 0 },
+  ]);
+  const navigate = useNavigate();
 
-  const handleClickAnswer = () => {
-    setQuestionNo(questionNo + 1);
+  useEffect(() => {
+    console.log("total", totalScore[0].score);
+  }, [totalScore[0].score]);
+
+  const handleClickAnswer = (ans: number, type: string) => {
+    const newScore = totalScore.map((s)=> s.id === type ? {id : s.id, score: s.score + ans} : s);
+    setTotalScore(newScore);
+    // 마지막 문제가 아닐경우
+    if(QuestionData.length !== questionNo +1) {
+      setQuestionNo(questionNo + 1);
+      //마지막 문제일 경우
+    } else {
+      navigate('/result')
+    }
   };
 
   return (
     <Wrapper>
-      <Header type="progress" questionNo={questionNo}/>
+      <Header type="progress" questionNo={questionNo} />
       <ContentsWrapper>
         <Title>{QuestionData[questionNo].title}</Title>
         <ButtonGroup>
           <Button
             variant="outline-success"
-            style={{ marginRight: "20px" }}
-            onClick={handleClickAnswer}
+            style={{
+              marginRight: "20px",
+              width: "45%",
+              minHeight: "200px",
+              fontSize: "15px",
+            }}
+            onClick={() => handleClickAnswer(1, QuestionData[questionNo].type)}
           >
             {QuestionData[questionNo].answera}
           </Button>
-          <Button variant="outline-success">
+          <Button
+            variant="outline-success"
+            style={{ width: "45%", minHeight: "200px", fontSize: "15px" }}
+            onClick={() => handleClickAnswer(0, QuestionData[questionNo].type)}
+          >
             {QuestionData[questionNo].answerb}
           </Button>
         </ButtonGroup>
